99537354d80caa5e67d3408695efc7107e29c668
Johnny Hooyberghs <7031043+Djohnnie@users.noreply.github.com>
2024-01-18 18:39:47 +0100

    Added Dockerfiles and pipeline

---
+++ MijnThuis.Application/Solar/Query/GetSolarOverviewQueryHandler.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MijnThuis.Application.Solar.Query;
+
+public class GetSolarOverviewQueryHandler
+{
+
+}
