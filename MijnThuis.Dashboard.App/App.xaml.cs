bf16621093dd815b2025a138b031ad1dea0ac8ff
Johnny Hooyberghs <7031043+Djohnnie@users.noreply.github.com>
2024-02-12 19:33:12 +0100

    Fixed Blazor page reload on Android

--- MijnThuis.Dashboard.App/App.xaml.cs
+++ MijnThuis.Dashboard.App/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿namespace MijnThuis.Dashboard.App;
+﻿
+namespace MijnThuis.Dashboard.App;
 
 public partial class App : Application
 {
@@ -7,5 +8,20 @@ public partial class App : Application
         InitializeComponent();
 
         MainPage = mainPage;
+    }
+
+    protected override Window CreateWindow(IActivationState? activationState)
+    {
+        var window = base.CreateWindow(activationState);
+
+        window.Activated += (sender, args) =>
+        {
+            if (MainPage is MainPage mainPage)
+            {
+                mainPage.Reload();
+            }
+        };
+
+        return window;
     }
 }
--- MijnThuis.Dashboard.App/MainPage.xaml.cs
+++ MijnThuis.Dashboard.App/MainPage.xaml.cs
@@ -1,9 +1,16 @@
-﻿namespace MijnThuis.Dashboard.App;
+﻿
+namespace MijnThuis.Dashboard.App;
 
 public partial class MainPage : ContentPage
 {
     public MainPage()
     {
         InitializeComponent();
+    }
+
+    public void Reload()
+    {
+        mainWebView.Source = "http://192.168.10.2:8809/?accessKey=cddf6b39-ebc4-4744-ad09-6c486c28120d";
+        mainWebView.Reload();
     }
 }
