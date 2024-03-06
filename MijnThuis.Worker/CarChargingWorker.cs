8b456d4a17ad42d1ddb509a333b823057403f188
Johnny Hooyberghs <7031043+Djohnnie@users.noreply.github.com>
2024-03-06 10:08:55 +0100

    Added some comments to the car charging logic

--- MijnThuis.Worker/CarChargingWorker.cs
+++ MijnThuis.Worker/CarChargingWorker.cs
@@ -32,6 +32,7 @@ public class CarChargingWorker : BackgroundService
 
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
+        // Initialize dependencies and variables.
         using var serviceScope = _serviceProvider.CreateScope();
         var carService = serviceScope.ServiceProvider.GetService<ICarService>();
         var solarService = serviceScope.ServiceProvider.GetService<ISolarService>();
@@ -42,32 +43,51 @@ public class CarChargingWorker : BackgroundService
         var maxPossibleCurrent = 0M;
         var carIsReadyToCharge = false;
 
+        // While the service is not requested to stop...
         while (!stoppingToken.IsCancellationRequested)
         {
+            // Use a timestamp to calculate the duration of the whole process.
             var startTimer = Stopwatch.GetTimestamp();
 
             _logger.LogInformation("Checking car charging state...");
 
             try
             {
+                // Get the information about the car.
                 var carOverview = await carService.GetOverview();
+
+                // Get the location of the car.
                 var carLocation = await carService.GetLocation();
 
+                // If the car is parked within the "Thuis" area and the charge port is open (probably connected to the house).
                 if (carLocation.Location == "Thuis" && carOverview.IsChargePortOpen)
                 {
                     _logger.LogInformation("The car is located at 'Thuis' and the charge port is open!");
 
                     carIsReadyToCharge = true;
+
+                    // Get information about solar energy.
                     var solarOverview = await solarService.GetOverview();
+
+                    // Add the current solar power (in kW) to a list for calculating the average.
                     currentSolarPower.Add(solarOverview.CurrentSolarPower);
 
+                    // If 10 measurements have been collected, we can calculate an average.
                     if (currentSolarPower.Count == 10)
                     {
+                        // Calculate the average solar power in kW.
                         currentAverageSolarPower = currentSolarPower.Average();
+
+                        // Calculate the available solar power, by subtracting the current consumption
+                        // and adding the power used by the car charging (if it is charging).
                         currentAvailableSolarPower = currentAverageSolarPower - solarOverview.CurrentConsumptionPower + (carOverview.IsCharging ? carOverview.ChargingAmps * 230M / 1000M : 0M);
+                        
+                        // Calculate the maximum current based on the available solar power.
                         maxPossibleCurrent = currentAvailableSolarPower * 1000M / 230M;
                         _logger.LogInformation($"Battery charge level: {solarOverview.BatteryLevel} %");
                         _logger.LogInformation($"Average solar power past minute: {currentAverageSolarPower:F2} kW [{string.Join(';', currentSolarPower.Select(x => $"{x:F2}"))}]");
+                       
+                        // If the car is already charging, log the current charging amps.
                         if (carOverview.IsCharging)
                         {
                             _logger.LogInformation($"Car is charging: {carOverview.ChargingAmps}A");
@@ -79,14 +99,24 @@ public class CarChargingWorker : BackgroundService
                         _logger.LogInformation($"Remaining power: Average solar power {currentAverageSolarPower:F2} kW - Current consumption {solarOverview.CurrentConsumptionPower:F2} kW + Car charging power {carOverview.ChargingAmps * 230M / 1000M} kW");
                         _logger.LogInformation($"Remaining power: {currentAvailableSolarPower:F2} kW");
                         _logger.LogInformation($"Maximum current available: {maxPossibleCurrent:F2} A");
+                        
+                        // Clear the list for calculating the average to
+                        // prepare it for the next calculation.
                         currentSolarPower.Clear();
 
+                        // If the home battery is charged over 95% and the car is not charging
+                        // or the car is charging at a different charging amps level and the
+                        // available charging amps level is higher or equal to 2A: Start charging
+                        // the car at the maximum possible current.
                         if (solarOverview.BatteryLevel > 95 && (!carOverview.IsCharging || carOverview.ChargingAmps != (int)maxPossibleCurrent) && (int)maxPossibleCurrent >= 2)
                         {
                             _logger.LogInformation($"Start charging at {(int)maxPossibleCurrent} A");
                             await carService.StartCharging((int)maxPossibleCurrent);
                         }
 
+                        // If the car is already charging and the current home battery level has
+                        // dropped below 95% or the maximum possible current has dropped below 2A:
+                        // Stop charging the car.
                         if (carOverview.IsCharging && (solarOverview.BatteryLevel < 95 || (int)maxPossibleCurrent < 2))
                         {
                             _logger.LogInformation("Stop charging!");
@@ -105,7 +135,12 @@ public class CarChargingWorker : BackgroundService
                 _logger.LogError(ex, ex.Message);
             }
 
+            // Calculate the duration for this whole process.
             var stopTimer = Stopwatch.GetTimestamp();
+
+            // If the car is ready to charge, wait for a total of 10 seconds before the next
+            // iteration. If the car is not ready to charge (because it is not parked near the
+            // house or it is not connected), wait for 5 minuted before the next iteration.
             var duration = (carIsReadyToCharge ? TimeSpan.FromSeconds(10) : TimeSpan.FromMinutes(5)) - TimeSpan.FromTicks(stopTimer - startTimer);
 
             if (duration > TimeSpan.Zero)
