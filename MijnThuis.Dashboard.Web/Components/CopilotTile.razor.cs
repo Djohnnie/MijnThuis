d74456532f656b3783c5c9ef6b24260e45baee00
Johnny Hooyberghs <7031043+Djohnnie@users.noreply.github.com>
2024-05-19 16:34:26 +0200

    Implemented Speech-To-Text using Azure Cognitive Services

--- MijnThuis.Dashboard.Web/Components/CopilotTile.razor.cs
+++ MijnThuis.Dashboard.Web/Components/CopilotTile.razor.cs
@@ -1,3 +1,4 @@
+using Microsoft.JSInterop;
 using MijnThuis.Dashboard.Web.Copilot;
 
 namespace MijnThuis.Dashboard.Web.Components;
@@ -5,6 +6,17 @@ namespace MijnThuis.Dashboard.Web.Components;
 public partial class CopilotTile
 {
     public string Prompt { get; set; }
+    public string TTSKey { get; set; }
+    public string TTSRegion { get; set; }
+    public string TTSLanguage { get; set; }
+
+    protected override void OnInitialized()
+    {
+        var config = ScopedServices.GetRequiredService<IConfiguration>();
+        TTSKey = config.GetValue<string>("SPEECH_API_KEY");
+        TTSRegion = config.GetValue<string>("SPEECH_REGION");
+        TTSLanguage = config.GetValue<string>("SPEECH_LANGUAGE");
+    }
 
     public async Task ExecutePrompt()
     {
@@ -13,6 +25,14 @@ public partial class CopilotTile
         var prompt = Prompt;
         Prompt = "De MijnThuis Copilot is aan het nadenken...";
         Prompt = await copilotHelper.ExecutePrompt(prompt);
+
+        StateHasChanged();
+    }
+
+    public async Task ExecuteTTS()
+    {
+        var result = await JS.InvokeAsync<string>("executeTextToSpeech", TTSLanguage, TTSKey, TTSRegion);
+        Prompt = result;
 
         StateHasChanged();
     }
