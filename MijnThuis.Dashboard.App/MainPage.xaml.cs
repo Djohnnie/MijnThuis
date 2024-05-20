1bf6fd82f095951652b006f3e7edb68ce9b4801b
Johnny Hooyberghs <7031043+Djohnnie@users.noreply.github.com>
2024-05-20 15:05:12 +0200

    Fixed Android WebView permissions

--- MijnThuis.Dashboard.App/MainPage.xaml.cs
+++ MijnThuis.Dashboard.App/MainPage.xaml.cs
@@ -1,4 +1,4 @@
-﻿
+﻿using Microsoft.Maui.Handlers;
 using MijnThuis.Dashboard.App.Configuration;
 
 namespace MijnThuis.Dashboard.App;
@@ -17,5 +17,12 @@ public partial class MainPage : ContentPage
     {
         mainWebView.Source = $"https://thuis.djohnnie.be:8809/?accessKey={_clientConfiguration.AccessKey}";
         mainWebView.Reload();
+    }
+
+    private void ContentPage_Loaded(object sender, EventArgs e)
+    {
+#if ANDROID
+        ((IWebViewHandler)mainWebView.Handler).PlatformView.SetWebChromeClient(new MijnThuis.Dashboard.App.Platforms.Android.MyWebChromeClient());
+#endif
     }
 }
---
+++ MijnThuis.Dashboard.App/Platforms/Android/MyWebChromeClient.cs
@@ -0,0 +1,21 @@
+﻿using Android.Webkit;
+
+namespace MijnThuis.Dashboard.App.Platforms.Android;
+
+internal class MyWebChromeClient : WebChromeClient
+{
+    public override void OnPermissionRequest(PermissionRequest request)
+    {
+
+        foreach (var resource in request.GetResources())
+        {
+            if (resource.Equals(PermissionRequest.ResourceAudioCapture, StringComparison.OrdinalIgnoreCase))
+            {
+                request.Grant(request.GetResources());
+                return;
+            }
+        }
+
+        base.OnPermissionRequest(request);
+    }
+}
