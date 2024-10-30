f22d662da82ed0e83473fe390e179f34c8551086
Djohnnie <7031043+Djohnnie@users.noreply.github.com>
2024-10-30 16:56:54 +0100

    Added some minor tweaks

--- MijnThuis.Worker/HomeBatteryChargingWorker.cs
+++ MijnThuis.Worker/HomeBatteryChargingWorker.cs
@@ -31,6 +31,7 @@ internal class HomeBatteryChargingWorker : BackgroundService
         const decimal LATITUDE = 51.06M;
         const decimal LONGITUDE = 4.36M;
         const int DELAY_IN_HOURS = 1;
+        const int CHARGING_POWER = 1500;
 
         DateTime? charged = null;
 
@@ -58,24 +59,24 @@ internal class HomeBatteryChargingWorker : BackgroundService
 
                     var batteryLevel = await modbusService.GetBatteryLevel();
 
+                    var zw6 = await forecastService.GetSolarForecastEstimate(LATITUDE, LONGITUDE, 39M, 43M, 2.5M);
+                    var no3 = await forecastService.GetSolarForecastEstimate(LATITUDE, LONGITUDE, 39M, -137M, 1.2M);
+                    var zo4 = await forecastService.GetSolarForecastEstimate(LATITUDE, LONGITUDE, 10M, -47M, 1.6M);
+                    var totalWattHoursEstimate = zw6.EstimatedWattHoursToday + no3.EstimatedWattHoursToday + zo4.EstimatedWattHoursToday;
+
+                    _logger.LogInformation($"Total estimated solar energy today: {zw6.EstimatedWattHoursToday}Wh + {no3.EstimatedWattHoursToday}Wh + {zo4.EstimatedWattHoursToday}Wh = {totalWattHoursEstimate}Wh");
+
                     if (batteryLevel.Level < 20)
                     {
                         _logger.LogInformation($"Battery level is below 20%: Start charging at {DateTime.Now}!");
 
-                        var zw6 = await forecastService.GetSolarForecastEstimate(LATITUDE, LONGITUDE, 39M, 43M, 2.5M);
-                        var no3 = await forecastService.GetSolarForecastEstimate(LATITUDE, LONGITUDE, 39M, -137M, 1.2M);
-                        var zo4 = await forecastService.GetSolarForecastEstimate(LATITUDE, LONGITUDE, 10M, -47M, 1.6M);
-                        var totalWattHoursEstimate = zw6.EstimatedWattHoursToday + no3.EstimatedWattHoursToday + zo4.EstimatedWattHoursToday;
-
-                        _logger.LogInformation($"Total estimated solar energy today: {zw6.EstimatedWattHoursToday}Wh + {no3.EstimatedWattHoursToday}Wh + {zo4.EstimatedWattHoursToday}Wh = {totalWattHoursEstimate}Wh");
-
-                        var wattHoursToCharge = 9700 - batteryLevel.Level * 97 - totalWattHoursEstimate;
+                        var wattHoursToCharge = 9700 - batteryLevel.Level * 97 - totalWattHoursEstimate + 1000;
 
                         if (wattHoursToCharge > 0)
                         {
                             _logger.LogInformation($"Battery should charge {wattHoursToCharge}Wh!");
 
-                            var durationInSeconds = wattHoursToCharge * 3.6M;
+                            var durationInSeconds = wattHoursToCharge * 3600M / CHARGING_POWER;
                             durationInSeconds = durationInSeconds > 18000 ? 18000 : durationInSeconds;
                             var chargingDuration = TimeSpan.FromSeconds((double)durationInSeconds);
 
@@ -85,7 +86,7 @@ internal class HomeBatteryChargingWorker : BackgroundService
                                 await Task.Delay(TimeSpan.FromSeconds(10), stoppingToken);
 
                                 // Charge the battery with the remaining watt hours.
-                                await modbusService.StartChargingBattery(chargingDuration, 1000);
+                                await modbusService.StartChargingBattery(chargingDuration, CHARGING_POWER);
                             }
                             catch
                             {
@@ -130,86 +131,5 @@ internal class HomeBatteryChargingWorker : BackgroundService
                 await Task.Delay(TimeSpan.FromMinutes(5));
             }
         }
-
-
-        // (20/10/2024) 2354 + 832 + 1384 = 4570
-
-        //Console.WriteLine(result.EstimatedWattHoursToday);
-
-        //using var modbusClient = new ModbusClient("x.x.x.x", 0);
-
-        //await modbusClient.Connect();
-        //var storageControlMode = await modbusClient.ReadHoldingRegisters<Djohnnie.SolarEdge.ModBus.TCP.Types.UInt16>(SunspecConsts.Storage_Control_Mode);
-        //if (storageControlMode.Value != 1)
-        //{
-        //    await modbusClient.WriteSingleRegister(SunspecConsts.Storage_Control_Mode, 1);
-        //}
-
-        //DateTime? startDateTime = null;
-        //bool isCharging = false;
-
-        //float chargeStarted = 0;
-        //float energyAdded = 0;
-        //float chargeAdded = 0;
-
-        //// While the service is not requested to stop...
-        //while (!stoppingToken.IsCancellationRequested)
-        //{
-        //    try
-        //    {
-        //        storageControlMode = await modbusClient.ReadHoldingRegisters<Djohnnie.SolarEdge.ModBus.TCP.Types.UInt16>(SunspecConsts.Storage_Control_Mode);
-        //        var storagePower = await modbusClient.ReadHoldingRegisters<Djohnnie.SolarEdge.ModBus.TCP.Types.Float32>(SunspecConsts.Battery_1_Instantaneous_Power);
-        //        var storageState = await modbusClient.ReadHoldingRegisters<Djohnnie.SolarEdge.ModBus.TCP.Types.Float32>(SunspecConsts.Battery_1_State_of_Energy);
-
-
-
-        //        // Charge from grid during 5 minutes and pause for 10 minutes.
-        //        if (!startDateTime.HasValue && !isCharging)
-        //        {
-        //            Console.WriteLine($"Start charging at {DateTime.Now}");
-        //            await modbusClient.WriteSingleRegister(SunspecConsts.Storage_Control_Mode, 2);
-        //            startDateTime = DateTime.Now;
-        //            isCharging = true;
-        //            chargeStarted = storageState.Value;
-        //        }
-
-        //        if (startDateTime.HasValue && isCharging)
-        //        {
-        //            if (DateTime.Now - startDateTime.Value > TimeSpan.FromMinutes(5))
-        //            {
-        //                Console.WriteLine($"STOP charging at {DateTime.Now}");
-        //                await modbusClient.WriteSingleRegister(SunspecConsts.Storage_Control_Mode, 1);
-        //                isCharging = false;
-        //                chargeAdded = storageState.Value - chargeStarted;
-        //                energyAdded = storagePower.Value / 60f * 5f;
-        //                chargeStarted = 0;
-        //                Console.WriteLine($"{energyAdded}Wh added, {chargeAdded}% added");
-        //            }
-        //        }
-
-        //        if (startDateTime.HasValue && !isCharging)
-        //        {
-        //            if (DateTime.Now - startDateTime.Value > TimeSpan.FromMinutes(15))
-        //            {
-        //                Console.WriteLine($"Start charging at {DateTime.Now}");
-        //                await modbusClient.WriteSingleRegister(SunspecConsts.Storage_Control_Mode, 2);
-        //                startDateTime = DateTime.Now;
-        //                isCharging = true;
-        //                chargeStarted = storageState.Value;
-        //            }
-        //        }
-
-
-
-
-        //        Console.WriteLine($"{storagePower}W ({storageState}%, {storageControlMode})");
-
-        //        await Task.Delay(10000);
-        //    }
-        //    catch (Exception ex)
-        //    {
-        //        _logger.LogError(ex, ex.Message);
-        //    }
-        //}
     }
 }
