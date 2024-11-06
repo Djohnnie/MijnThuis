f10a550fb20dcc1b55d4c370ba6f1581065dcdf7
Djohnnie <7031043+Djohnnie@users.noreply.github.com>
2024-11-06 14:43:38 +0100

    Changed charger service to use the public API from Shell Recharge

--- MijnThuis.Integrations/Car/ChargerService.cs
+++ MijnThuis.Integrations/Car/ChargerService.cs
@@ -1,5 +1,4 @@
 ﻿using Microsoft.Extensions.Configuration;
-using System.Net.Http.Headers;
 using System.Net.Http.Json;
 using System.Text.Json.Serialization;
 
@@ -12,26 +11,31 @@ public interface IChargerService
 
 public class ChargerService : BaseChargerService, IChargerService
 {
-    private readonly string _sessionId;
-
-    public ChargerService(IConfiguration configuration) : base(configuration)
-    {
-        _sessionId = configuration.GetValue<string>("CHARGER_SESSION_ID");
-    }
+    public ChargerService(IConfiguration configuration) : base(configuration) { }
 
     public async Task<ChargerOverview> GetChargerOverview(string chargerId)
     {
         using var client = InitializeHttpClient();
 
-        var response = await client.GetFromJsonAsync<GetChargersResponse>($"1/get_chargers?ids={chargerId}&get_status=true&session_id={_sessionId}");
-        var result = response.Chargers.Single(x => $"{x.Id}" == chargerId);
+        var response = await client.GetFromJsonAsync<GetChargersResponse>($"api/chargepoint?id={chargerId}");
+
+        if (response.Chargepoint != null)
+        {
+            return new ChargerOverview
+            {
+                ChargerId = $"{response.Chargepoint.Id}",
+                Description = response.Chargepoint.Name,
+                NumberOfChargers = response.Chargepoint.Connectors.Count,
+                NumberOfChargersAvailable = response.Chargepoint.Connectors.Count(x => x.Status == "Available")
+            };
+        }
 
         return new ChargerOverview
         {
-            ChargerId = $"{result.Id}",
-            Description = result.Name,
-            NumberOfChargers = result.Connectors.Count,
-            NumberOfChargersAvailable = result.Connectors.Count(x => x.Status == "AVAILABLE")
+            ChargerId = chargerId,
+            Description = "Unknown",
+            NumberOfChargers = 0,
+            NumberOfChargersAvailable = 0
         };
     }
 }
@@ -39,12 +43,10 @@ public class ChargerService : BaseChargerService, IChargerService
 public class BaseChargerService
 {
     private readonly string _baseAddress;
-    private readonly string _apiKey;
 
     protected BaseChargerService(IConfiguration configuration)
     {
         _baseAddress = configuration.GetValue<string>("CHARGER_API_BASE_ADDRESS");
-        _apiKey = configuration.GetValue<string>("CHARGER_API_KEY");
     }
 
     protected HttpClient InitializeHttpClient()
@@ -54,27 +56,25 @@ public class BaseChargerService
             BaseAddress = new Uri(_baseAddress)
         };
 
-        client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("APIKEY", _apiKey);
-
         return client;
     }
 }
 
 public class GetChargersResponse
 {
-    [JsonPropertyName("result")]
-    public List<ChargerResponse> Chargers { get; set; }
+    [JsonPropertyName("chargepoint")]
+    public Chargepoint Chargepoint { get; set; }
 }
 
-public class ChargerResponse
+public class Chargepoint
 {
-    [JsonPropertyName("name_without_network")]
+    [JsonPropertyName("uid")]
+    public int Id { get; set; }
+
+    [JsonPropertyName("externalId")]
     public string Name { get; set; }
 
-    [JsonPropertyName("id")]
-    public long Id { get; set; }
-
-    [JsonPropertyName("outlets")]
+    [JsonPropertyName("evses")]
     public List<Connector> Connectors { get; set; }
 }
 
