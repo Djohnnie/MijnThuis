efa67a83eea5e0ad2366fd0a4bcd5a9326eed352
Djohnnie <7031043+Djohnnie@users.noreply.github.com>
2025-05-16 14:32:44 +0200

    First step to adding a Modbus proxy API

--- MijnThuis.ModbusProxy.Api/Program.cs
+++ MijnThuis.ModbusProxy.Api/Program.cs
@@ -1,6 +1,23 @@
 ﻿using MijnThuis.ModbusProxy.Api.Helpers;
+using System.Net;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.WebHost.ConfigureKestrel((context, options) =>
+{
+    var certificateFilename = context.Configuration.GetValue<string>("CERTIFICATE_FILENAME");
+    var certificatePassword = context.Configuration.GetValue<string>("CERTIFICATE_PASSWORD");
+    var port = context.Configuration.GetValue<int?>("PORT") ?? 8080;
+
+    if (certificateFilename == null)
+    {
+        options.Listen(IPAddress.Any, port);
+    }
+    else
+    {
+        options.Listen(IPAddress.Any, port, listenOption => listenOption.UseHttps(certificateFilename, certificatePassword));
+    }
+});
+
 builder.Services.AddSingleton<IModbusHelper, ModbusHelper>();
 
 var app = builder.Build();
