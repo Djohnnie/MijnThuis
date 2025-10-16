482b61ef0dac7a08c0b817928b506593f4de0b04
Djohnnie <7031043+Djohnnie@users.noreply.github.com>
2025-10-16 16:56:16 +0200

    Added cheapest day-ahead energy prices

--- MijnThuis.Worker/Program.cs
+++ MijnThuis.Worker/Program.cs
@@ -10,18 +10,18 @@ builder.Services.AddIntegrations();
 builder.Services.AddScoped<ICarChargingHelper, CarChargingHelper>();
 builder.Services.AddScoped<IHomeBatteryChargingHelper, HomeBatteryChargingHelper>();
 
-//builder.Services.AddHostedService<PowerPeakNotificationWorker>();
-//builder.Services.AddHostedService<CarChargingWorker>();
+builder.Services.AddHostedService<PowerPeakNotificationWorker>();
+builder.Services.AddHostedService<CarChargingWorker>();
 builder.Services.AddHostedService<HomeBatteryChargingWorker>();
-//builder.Services.AddHostedService<HomeBatteryNotificationWorker>();
-//builder.Services.AddHostedService<SolarHistoryWorker>();
-//builder.Services.AddHostedService<EnergyHistoryWorker>();
-//builder.Services.AddHostedService<SolarForecastHistoryWorker>();
-//builder.Services.AddHostedService<DayAheadEnergyPricesWorker>();
-//builder.Services.AddHostedService<InjectionWithCostWorker>();
-//builder.Services.AddHostedService<CarHistoryWorker>();
-//builder.Services.AddHostedService<CarStatusWorker>();
-//builder.Services.AddHostedService<SamsungTheFrameWorker>();
+builder.Services.AddHostedService<HomeBatteryNotificationWorker>();
+builder.Services.AddHostedService<SolarHistoryWorker>();
+builder.Services.AddHostedService<EnergyHistoryWorker>();
+builder.Services.AddHostedService<SolarForecastHistoryWorker>();
+builder.Services.AddHostedService<DayAheadEnergyPricesWorker>();
+builder.Services.AddHostedService<InjectionWithCostWorker>();
+builder.Services.AddHostedService<CarHistoryWorker>();
+builder.Services.AddHostedService<CarStatusWorker>();
+builder.Services.AddHostedService<SamsungTheFrameWorker>();
 
 var host = builder.Build();
 
